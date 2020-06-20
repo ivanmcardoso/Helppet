a1f7df92a656bb546b30be205cccaf149bdae55f
IVAN MILLER MEDEIROS CARDOSO <immc.hef@uea.edu.br>
2020-06-20 14:42:45 -0400

    chore: merge with denuncia

--- app/src/main/java/com/hefesto/helppet/activity/OngsActivity.kt
+++ app/src/main/java/com/hefesto/helppet/activity/OngsActivity.kt
@@ -1,9 +1,10 @@
-package com.hefesto.helppet
+package com.hefesto.helppet.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hefesto.helppet.R
 
-class ongs : AppCompatActivity() {
+class OngsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ongs)
--- app/src/main/java/com/hefesto/helppet/fragment/DenunciaFragment.kt
+++ app/src/main/java/com/hefesto/helppet/fragment/DenunciaFragment.kt
@@ -1,11 +1,14 @@
 package com.hefesto.helppet.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.hefesto.helppet.R
+import com.hefesto.helppet.activity.OngsActivity
+import kotlinx.android.synthetic.main.fragment_denuncia.*
 
 class DenunciaFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
@@ -13,5 +16,6 @@ class DenunciaFragment: Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        btDenunciaOngs.setOnClickListener { Intent(context, OngsActivity::class.java).also { startActivity(it) } }
     }
 }
